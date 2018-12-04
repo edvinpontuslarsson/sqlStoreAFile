e0e842193c6b30f308f583d64c09c4583ba2d9eb
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-12-04 16:28:37 +0100

    Changes & only resolve as param for Promise

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict'
 
 const getFilePath = require('./lib/getFilePath')
-const getValues = require('./lib/getValues')
+const getObjects = require('./lib/getObjects')
 const storeValues = require('./lib/storeValues')
 
 ;(async () => {
   let startTime = new Date()
 
   const filePath = getFilePath()
-  const values = await getValues(filePath)
-  storeValues(values, startTime)
+  const objects = await getObjects(filePath)
+  storeValues(objects, startTime)
 })()
