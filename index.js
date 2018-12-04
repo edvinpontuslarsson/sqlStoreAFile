a03e60314bd53e903f1a73a2a791fb9457ee17c3
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-12-04 15:21:21 +0100

    standard fix

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const getValues = require('./lib/getValues')
 const storeValues = require('./lib/storeValues')
 
 ;(async () => {
-    let startTime = new Date()
+  let startTime = new Date()
 
-    const filePath = getFilePath()
-    const values = await getValues(filePath)
-    storeValues(values, startTime)
+  const filePath = getFilePath()
+  const values = await getValues(filePath)
+  storeValues(values, startTime)
 })()
