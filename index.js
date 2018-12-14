0bb09783d20327905218775caf7d0aebcbf9fc7d
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-12-14 16:06:31 +0100

    Back to just having 1 table

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict'
 
 const getFilePath = require('./lib/getFilePath')
-const getObjects = require('./lib/getObjects')
+const getValues = require('./lib/getValues')
 const storeValues = require('./lib/storeValues')
 
 ;(async () => {
   let startTime = new Date()
 
   const filePath = getFilePath()
-  const objects = await getObjects(filePath)
-  storeValues(objects, startTime)
+  const values = await getValues(filePath)
+  storeValues(values, startTime)
 })()
